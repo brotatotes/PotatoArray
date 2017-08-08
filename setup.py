d65ab5d8001b2f7fb7725f5e982eb522bf4dd643
brotatotes <hao.awesome@gmail.com>
2017-08-08 19:17:50 -0400

    update gitignore and package files

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup
 
 setup(
 	name = 'PotatoArray',
-	version = '0.1',
+	version = '0.2',
 	scripts = ['PotatoArray.py'],
 	author = 'Eric Hao',
-	description = ("Arrays start at 2.")
+	author_email = 'erichao2018@gmail.com',
+	description = ("Arrays start at 2."),
+	url = "https://github.com/brotatotes/PotatoArray"
 )
